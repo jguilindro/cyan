19d1e1002680600921576a89f481818a1af7c7d6
Julio Guilindro <julio.guilindro@gmail.com>
2020-07-13 18:45:08 -0400

    Added Visual Component to main screen

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../styles/App.css';
 import PianoComponent from './PianoComponent'
+import VisualComponent from './VisualComponent'
 
 function App() {
   return (
     <div id="App" className="App">
+      <VisualComponent></VisualComponent>
       <PianoComponent></PianoComponent>
     </div>
   );
--- src/components/VisualComponent.js
+++ src/components/VisualComponent.js
@@ -1,31 +1,60 @@
 import React from 'react';
-import Sketch from 'react-p5';
+import p5 from 'p5';
+import '../styles/Visual.css';
 
 class VisualComponent extends React.Component {
-	y = 0;
-	direction = '^';
+	constructor(props) {
+		super(props);
+		this.state = {
+			visualWidth: window.innerWidth,
+			visualHeight: window.innerHeight - 90
+		}
+		this.visualRef = React.createRef();
+	}
+
+	Sketch = (p5) => {
+		let y = 0;
+		let direction = '^';
+		p5.setup = () => {
+			p5.createCanvas(this.state.visualWidth, this.state.visualHeight);
+		}
+
+		p5.draw = () => {
+			p5.background(0);
+			p5.fill(255, y * 1.3, 0);
+			p5.ellipse(p5.width / 2, y, 50);
+			if (y > p5.height) direction = '';
+			if (y < 0) {
+				direction = '^';
+			}
+			if (direction === '^') y += 8;
+			else y -= 4;
+
+		}
+		p5.windowResized = () => {
+			p5.resizeCanvas(this.state.visualWidth, this.state.visualHeight);
+		}
+	}
+
+	onResize() {
+		this.setState({
+			visualHeight: window.innerHeight - 90,
+			visualWidth: window.innerWidth
+		});
+	}
+
+	componentDidMount() {
+		window.addEventListener('resize', this.onResize.bind(this));
+		this.visualPanel = new p5(this.Sketch, this.visualRef.current);
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener('resize', this.onResize.bind(this));
+	}
 
 	render() {
 		return (
-			<div className="VisualComponent">
-				<Sketch
-					setup={(p5, parentRef) => {
-						p5.createCanvas(200, 200).parent(parentRef);
-					}}
-					draw={p5 => {
-
-						p5.background(0);
-						p5.fill(255, this.y * 1.3, 0);
-						p5.ellipse(p5.width / 2, this.y, 50);
-						if (this.y > p5.height) this.direction = '';
-						if (this.y < 0) {
-							this.direction = '^';
-						}
-						if (this.direction === '^') this.y += 8;
-                        else this.y -= 4;
-                        
-					}}
-				/>
+			<div className="VisualComponent" ref={this.visualRef}>
 			</div>
 		);
 	}
